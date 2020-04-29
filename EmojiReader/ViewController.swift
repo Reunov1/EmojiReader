d34f1b9fd30013b298a045d44329081af9dc127e
Aleksandr Reunov <reun_alex@mail.ru>
2020-04-29 03:29:38 +0300

    Initial Commit

---
+++ EmojiReader/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiReader
+//
+//  Created by Александр Реунов on 29.04.2020.
+//  Copyright © 2020 Александр Реунов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
